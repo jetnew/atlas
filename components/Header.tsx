0ad0122a04fc1d85c0b65f8910f73cce75364cce
Jet New <notesjet@gmail.com>
2025-12-22 12:28:42 +0800

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,32 +2,22 @@
 
 import { Profile } from "@/components/Profile";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
 import Link from "next/link";
+import { useProject } from "@/components/ProjectContext";
 
 export default function Header() {
+  const { currentProject } = useProject();
+  const projectTitle = currentProject?.map?.report?.title || "Atlas";
+
   return (
     <header className="sticky top-0 z-50 flex w-full items-center">
-      <div className="flex h-(--header-height) w-full items-center gap-4 px-4">
+      <div className="flex h-(--header-height) w-full items-center gap-3 px-4">
         <Link href="/" className="font-bold text-xl cursor-pointer">
           <Avatar>
             <AvatarImage src="/logo.webp" alt="Atlas" />
           </Avatar>
         </Link>
-        <Breadcrumb className="hidden sm:block">
-          <BreadcrumbList>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Atlas</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <h1 className="text-sm">{projectTitle}</h1>
         <Profile />
       </div>
     </header>
